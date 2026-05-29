cdad1a855d4924a3ee1ce758765199db239795b3
cleytoncasanova4-star <cleytoncasanova4@gmail.com>
2026-05-29 17:19:50 +0200

    Criar Pagina de Noticia

---
+++ Tech News/js/noticias.js
@@ -0,0 +1,60 @@
+let todasNoticias = [];
+
+async function carregarNoticias() {
+  const { data, error } = await supabaseClient
+    .from("noticias")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  todasNoticias = data;
+  renderNoticias(data);
+}
+
+function renderNoticias(data) {
+  const container = document.getElementById("news-container");
+
+  if (!container) return;
+
+  container.innerHTML = "";
+
+  data.forEach((n) => {
+    container.innerHTML += `
+      <div class="card">
+
+        <img src="${n.imagem}" onclick="abrirNoticia(${n.id})">
+
+        <div class="card-content">
+
+          <span>${n.categoria}</span>
+
+          <h3>${n.titulo}</h3>
+
+          <p>${n.resumo}</p>
+
+        </div>
+
+      </div>
+    `;
+  });
+}
+
+function filtrar(cat) {
+  if (cat === "TODOS") {
+    renderNoticias(todasNoticias);
+    return;
+  }
+
+  const filtradas = todasNoticias.filter((n) => n.categoria === cat);
+  renderNoticias(filtradas);
+}
+
+function abrirNoticia(id) {
+  window.location.href = `noticias.html?id=${id}`;
+}
+
+window.addEventListener("DOMContentLoaded", carregarNoticias);
