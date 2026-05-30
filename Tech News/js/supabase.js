c04ba6ee60833aed6e95db6116ff2e82d575a8bc
cleytoncasanova4-star <cleytoncasanova4@gmail.com>
2026-05-30 11:45:03 +0200

    corrigida estrutura do projeto

--- Tech News/js/supabase.js
+++
@@ -1,9 +0,0 @@
-const supabaseUrl = "https://dycuzuewpufogzclsogz.supabase.co";
-
-const supabaseKey = "sb_publishable_UEXqjwaEtyWwHqLFPx4U_w_z2g7tY_g";
-
-const supabaseClient = 
-supabase.createClient(
-  supabaseUrl,
-   supabaseKey
-);
